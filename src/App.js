f4593b5c1d953acbb5d417be0f2f1358d4cd3d96
M-Isak-A <152233524+MohamedIsakAbdi@users.noreply.github.com>
2024-12-08 15:35:35 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
-import Navbar from './components/Navbar';
+
 
 
 
